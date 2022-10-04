886eba631b69df204c37adb67e3a886cc526c842
Vinza KJ <vinzkauzarj@gmail.com>
2022-10-04 16:30:02 +0700

    Progress Latihan1

---
+++ src/main/kotlin/datatype.kt
@@ -0,0 +1,19 @@
+const val KONSTAN_DATA = "Data" //konstan berada di luar function
+
+fun main() {
+    val dataInt = 5
+    val dataString = "Hai Polban"
+    val dataBoolean = true
+
+    var dataIntVar = 10
+
+    //dataInt = 6
+    dataIntVar = 10
+
+
+
+   println(dataInt.plus(other = 3))
+   println(dataInt)
+    println(dataIntVar)
+
+}
